6f9cc8e31d8d2ce344fc3f2f9d940935b230539a
zhang_xfei <1602725012@qq.com>
2018-02-25 15:26:03 +0800

    test

---
+++ src/main/java/Login.java
@@ -0,0 +1,9 @@
+import cn.hutool.core.date.DateTime;
+
+public class Login {
+    public static void main(String[] args) {
+        System.out.println("it's just a test");
+        DateTime date =  DateTime.now();
+        System.out.println(date);
+    }
+}
